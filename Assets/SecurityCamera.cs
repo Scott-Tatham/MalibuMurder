4d2c3fd62c70149bf6fea782acecff501d797517
Jaxson Gould <JaxsonGould@gmail.com>
2018-06-29 22:51:44 +1000

    Map update
    yer

--- Assets/SecurityCamera.cs
+++ Assets/SecurityCamera.cs
@@ -14,23 +14,15 @@ public class SecurityCamera : MonoBehaviour
     [Range(0, -180.0f)]
     public float maxNegativeHorizontal = 0.0f;
 
-    //[Range(-180.0f, 180.0f)]
-    //public float fieldOfView = 0.0f;
-
-    private Camera camera;
 	// Use this for initialization
 	void Start ()
     {
-        camera = GetComponent<Camera>();
-        if (camera == null)
-        {
-            Debug.LogWarning("Yo ya didnt assign this to a camera you dunkus");
-        }
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //transform.rotation = Quaternion.Euler(0, fieldOfView, 0);
+
 	}
 }
