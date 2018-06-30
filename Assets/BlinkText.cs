37657bd36f39e2528bed1b5e5662bd9dfe3e599d
Jaxson Gould <JaxsonGould@gmail.com>
2018-07-01 00:23:25 +1000

    UI stuff. Menu and hint paper anims

---
+++ Assets/BlinkText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour
+{
+    public float blinkSpeed = 0.0f;
+    public string text;
+
+    private float timer = 0;
+    private Text textOutput;
+
+	// Use this for initialization
+	void Start ()
+    {
+        textOutput = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (timer < blinkSpeed)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            if (textOutput.text == text)
+                textOutput.text = "";
+            else
+                textOutput.text = text;
+
+
+        }
+	}
+}
